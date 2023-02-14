f09ab4ecd70e1d112599a1c9f328ea0a684f1841
anvika.yendela <anvika.yendela@atmecsadminsMBP.bbrouter>
2023-02-14 22:32:46 +0530

    small change in .csv code

--- FileHandling/test_xmltocsv.py
+++ FileHandling/test_xmltocsv.py
@@ -1,3 +1,4 @@
+from _csv import writer
 from pathlib import Path
 from xml.etree import ElementTree
 import csv
@@ -10,20 +11,31 @@ if Path('output.xml').exists():
 else:
     print("no file found")
 
-# CREATE CSV FILE
-csvfile = open(get_file_path('.csv'), 'w', encoding='utf-8')
-csvfile_writer = csv.writer(csvfile)
 
-# ADD THE HEADER TO CSV FILE
-csvfile_writer.writerow(["failures", "skipped", "tests", "timestamp"])
+if Path(get_file_path('.csv')).exists():
+    print('exists')
+else:
+    csvfile = open(get_file_path('.csv'), 'w', encoding='utf-8')
+
+    csvfile_writer = csv.writer(csvfile)
+    csvfile_writer.writerow(["failures", "skipped", "tests", "timestamp"])
 
 for testsuite in xml.findall("testsuite"):
 
     if testsuite:
-        failures = testsuite.get('failures')
-        skipped = testsuite.get('skipped')
-        tests = testsuite.get('tests')
-        timestamp = testsuite.get('timestamp')
-        csv_line = [failures, skipped, tests, timestamp]
-        csvfile_writer.writerow(csv_line)
-csvfile.close()
+
+        with open(get_file_path('.csv'), 'a', newline='\n') as f_object:
+            failures = testsuite.get('failures')
+            skipped = testsuite.get('skipped')
+            tests = testsuite.get('tests')
+            timestamp = testsuite.get('timestamp')
+            csv_line = [failures, skipped, tests, timestamp]
+            # Pass the CSV  file object to the writer() function
+            writer_object = writer(f_object)
+            # Result - a writer object
+            # Pass the data in the list as an argument into the writerow() function
+            writer_object.writerow(csv_line)
+            # Close the file object
+            f_object.close()
+
+
